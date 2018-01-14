86445b209eb8d74fa4dfb88222a8d6c0fbec4651
root <grisha@somoto.net>
2018-01-14 18:57:43 +0200

    remove

--- src/js/background.js
+++ src/js/background.js
@@ -2,8 +2,31 @@ import '../img/icon128.png'
 import '../img/icon48.png'
 import '../img/icon16.png'
 
-import { beforeRequest } from "./beforerequest";
+import BeforeRequest from "./beforerequest";
 
-chrome.webRequest.onBeforeRequest.addListener( beforeRequest,
-    { urls: ["http://www.pdfbaron.com/?q=*"] },
+
+
+const requestFilters = [
+  "http://www.pdfbaron.com/srch/?q=*",
+  "*://www.google.com/search*",
+  "*://www.google.co.il/search*"
+];
+
+
+chrome.tabs.getSelected(null, function(tab){
+
+  chrome.webRequest.onBeforeRequest.addListener( (details) => {
+
+      let beforeRequestFactory = new BeforeRequest(details);
+      beforeRequestFactory.exec();
+
+      let redirectUrl = beforeRequestFactory.redirectLink;
+
+      return { 'redirectUrl': redirectUrl };
+
+    },
+    { urls: requestFilters},
     ["blocking"]);
+
+});
+
--- src/js/content.js
+++ src/js/content.js
@@ -2,5 +2,13 @@
 import ContentFactory from './contentFactory';
 
 
-let factory = new ContentFactory(window.location.host);
-factory.exec();
+let factory = new ContentFactory( window.location );
+
+if(document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', () => {
+
+    factory.exec();
+
+  }, false);
+}
+
